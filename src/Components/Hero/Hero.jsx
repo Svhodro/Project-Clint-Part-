6bdb0624e4a371d48179a2c4b241654cc19eca27
Svhodro <rudrolipi@gmail.com>
2024-05-06 14:13:40 +0600

    docs- Add Hero Section

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,9 +1,38 @@
 import React from 'react'
+import img1 from '../../assets/Man.png'
+import img2 from '../../assets/woman.png'
+import img3 from '../../assets/kid.png'
+
 
 function Hero() {
-  return (
-    <div>Hero</div>
-  )
+    return (
+        <div >
+            <div className="carousel w-full md:rounded-md h-[40vw]">
+                <div id="slide1" className="carousel-item relative w-full">
+                    <img src={img1} className="w-full" />
+                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <a href="#slide3" className="btn btn-circle">❮</a>
+                        <a href="#slide2" className="btn btn-circle">❯</a>
+                    </div>
+                </div>
+                <div id="slide2" className="carousel-item relative w-full">
+                    <img src={img2} className="w-full" />
+                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <a href="#slide1" className="btn btn-circle">❮</a>
+                        <a href="#slide3" className="btn btn-circle">❯</a>
+                    </div>
+                </div>
+                <div id="slide3" className="carousel-item relative w-full">
+                    <img src={img3} className="w-full" />
+                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <a href="#slide2" className="btn btn-circle">❮</a>
+                        <a href="#slide1" className="btn btn-circle">❯</a>
+                    </div>
+                </div>              
+            </div>
+
+        </div>
+    )
 }
 
 export default Hero
--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import Nav from '../Components/Header/Nav'
+import Hero from '../Components/Hero/Hero'
 
 function Home() {
   return (
     <div className='md:px-10'>
       {/* navbar */}
       <Nav />
+      {/* carosel */}
+      <Hero />
+      {/* card */}
+     
     </div>
   )
 }
