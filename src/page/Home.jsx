b5f2a7f0a5de2d981e7f2301ba957a252233aecb
Svhodro <rudrolipi@gmail.com>
2024-05-06 21:52:04 +0600

    docs- Shopingcard Add in Home page

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Nav from '../Components/Header/Nav'
 import Hero from '../Components/Hero/Hero'
 import LatestCard from '../Components/Cards/Latest/LatestCard'
+import Shoping from '../Components/Cards/ShopingCard/Shoping'
 
 function Home() {
   return (
@@ -10,20 +11,30 @@ function Home() {
       <Nav />
       {/* line */}
       <div className="flex justify-center py-6">
-      <hr className='w-5/6 border-slate-600 '/>
+        <hr className='w-5/6 border-slate-600 ' />
       </div>
       {/* carosel */}
       <Hero />
       {/* card */}
       <div className="flex justify-center py-6">
-      <hr className='w-5/6 border-slate-600 '/>
+        <hr className='w-5/6 border-slate-600 ' />
       </div>
       {/* Latest section */}
       <p className='md:text-2xl pl-3 md:p-0 font-serif '>Latest Product</p>
-       {/* latest card's */}
-       <LatestCard/>
-      
-     
+      {/* latest card's */}
+      <LatestCard />
+
+      <div className="flex justify-center py-6">
+        <hr className='w-5/6 border-slate-600 ' />
+      </div>
+      {/* discount shower card  */}
+      <Shoping />
+      <div className="flex justify-center py-6">
+        <hr className='w-5/6 border-slate-600 ' />
+      </div> 
+        <p className='md:text-2xl pl-3 md:p-0 font-serif '>Shop By Category</p>
+  
+
     </div>
   )
 }
